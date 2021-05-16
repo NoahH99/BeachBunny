064e44fb941830c006506c6499a399c9084aa476
Noah Hendrickson <noah@noahhendrickson.com>
2021-05-16 16:06:25 -0400

    Added example tests

---
+++ bot/src/test/kotlin/com/noahhendrickson/beachbunny/bot/BeachBunnyTest.kt
@@ -0,0 +1,14 @@
+package com.noahhendrickson.beachbunny.bot
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BeachBunnyTest {
+
+    @Test
+    fun `this is an example test`() {
+        val expected = 5 + 5
+
+        assertThat(10).isEqualTo(expected)
+    }
+}
---
+++ database/src/test/kotlin/com/noahhendrickson/beachbunny/database/DatabaseTest.kt
@@ -0,0 +1,14 @@
+package com.noahhendrickson.beachbunny.database
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class DatabaseTest {
+
+    @Test
+    fun `this is an example test`() {
+        val expected = 5 + 5
+
+        Assertions.assertThat(10).isEqualTo(expected)
+    }
+}
